d23e2e739a6476863fe519353657c6d54f1a78ef
Jack Park <jackpark@gmail.com>
2020-10-17 14:46:58 -0700

    updated jar version

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-java -cp ./target/tqos-asr-documizer-agent-0.4.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.os.asr.documizer.Main
+java -cp ./target/tqos-asr-documizer-agent-0.5.0-SNAPSHOT-jar-with-dependencies.jar org.topicquests.os.asr.documizer.Main
